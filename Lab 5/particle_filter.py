d37af1274aea72060ff3e495dfe9114977b67065
Paras Jain <parasjain@parasjain.com>
2017-03-16 02:39:35 -0400

    Autograder pass

--- Lab 5/particle_filter.py
+++ Lab 5/particle_filter.py
@@ -29,6 +29,41 @@ def motion_update(particles, odom):
     return motion_particles
 
 
+def measurement_update_set_weights(particles, measured_marker_list, grid: CozGrid):
+    nresample = 0
+    weighting = []
+    for p in particles:
+        if len(measured_marker_list) > 0:
+            markers_visible_to_particle = p.read_markers(grid)
+            if not grid.is_free(p.x, p.y): # case 0 - off the grid
+                nresample += 1
+                weighting.append(0.0)
+            elif len(markers_visible_to_particle) == 0: # case 2 - zero weight particle
+                weighting.append(0.0)
+            else:
+                closest_markers = []
+                for cm in measured_marker_list:
+                    if len(markers_visible_to_particle) > 0:
+                        min_dist = min(markers_visible_to_particle, key=lambda m: grid_distance(cm[0], cm[1], m[0], m[1]))
+                        markers_visible_to_particle.remove(min_dist)
+                        closest_markers.append((cm, min_dist))
+
+                prob = 1.0
+                for cozmo_marker, close_marker in closest_markers:
+                    dist_between_markers = grid_distance(cozmo_marker[0], cozmo_marker[1], close_marker[0], close_marker[1])
+                    angle_between_markers = diff_heading_deg(cozmo_marker[2], close_marker[2])
+                    prob *= math.exp(-1.0 * (math.pow(dist_between_markers, 2) * 1.0 / (2 * math.pow(MARKER_TRANS_SIGMA, 2))
+                                         + math.pow(angle_between_markers, 2) * 1.0 / (2 * math.pow(MARKER_ROT_SIGMA, 2))))
+
+                if prob < 0.00000001:
+                    prob = 0
+                    nresample += 1
+
+                weighting.append(prob)
+
+    return nresample, weighting
+
+
 def measurement_update(particles, measured_marker_list, grid: CozGrid):
     """ Particle filter measurement update
 
@@ -53,20 +88,27 @@ def measurement_update(particles, measured_marker_list, grid: CozGrid):
         Returns: the list of particles represents belief p(x_{t} | u_{t})
                 after measurement update
     """
-    measured_particles = []
 
     # step 1 - set weights
-    valid_particles = filter(lambda x: grid.is_free(x[0], x[1]), particles)
-    for p in valid_particles:
-        markers_visible_to_particle = particles.read_markers(grid)
+    nresample, weighting = measurement_update_set_weights(particles, measured_marker_list, grid)
 
-        closest_marker = []
-        for cm in markers_visible_to_particle:
-            m = min(markers_visible_to_particle, key=lambda m: grid_distance(cm[0], cm[1], m[0], m[1]))
-            closest_marker.append((cm, m))
-            markers_visible_to_particle.remove(m)
+    # step 2a - equal weight in situations with no weights
+    norm_factor = sum(weighting)
+    if norm_factor == 0.0 or len(measured_marker_list) < 1:
+        weighting = [(1.0 / float(len(particles)))] * len(particles)
+    else:
+        weighting = [w / norm_factor for w in weighting]
 
-        prob = 1.0
-        for marker in closest_marker:
-            m = marker[0]
-            n = marker[1]
+    # step 2b - probabilistic re-sampling
+    NSAMPLE = min(500, min(len(particles), 50 + nresample))
+
+    samples = np.random.choice(particles, size = (len(particles) - NSAMPLE), p = weighting).tolist()
+    samples = [Particle(oldParticle.x, oldParticle.y, heading=oldParticle.h) for oldParticle in samples]
+
+    # step 2c - pad array with random values sampled iid
+    random_samples = [grid.random_free_place() for i in range(NSAMPLE)]
+    for x, y in random_samples:
+        samples.append(Particle(x, y))
+
+    return samples
+
