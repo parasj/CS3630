62b4b1a4ed9780bfbce4627e36ff9eae340c5300
Paras Jain <parasj96@gmail.com>
2017-04-18 15:40:43 -0400

    Move annotator

--- Lab 7/go_to_goal.py
+++ Lab 7/go_to_goal.py
@@ -134,6 +134,16 @@ class BallAnnotator(cozmo.annotate.Annotator):
             BallAnnotator.ball = None
 
 
+# Define a decorator as a subclass of Annotator; displays battery voltage
+class BatteryAnnotator(cozmo.annotate.Annotator):
+    def apply(self, image, scale):
+        d = ImageDraw.Draw(image)
+        bounds = (0, 0, image.width, image.height)
+        batt = self.world.robot.battery_voltage
+        text = cozmo.annotate.ImageText('BATT %.1fv' % batt, color='green')
+        text.render(d, bounds)
+
+
 # particle filter functionality
 class ParticleFilter:
     def __init__(self, grid):
@@ -244,45 +254,6 @@ async def run(robot: cozmo.robot.Robot):
                     
             elif state == 'lock-in':
                 print("lock-in")
-# Define a decorator as a subclass of Annotator; displays battery voltage
-class BatteryAnnotator(cozmo.annotate.Annotator):
-    def apply(self, image, scale):
-        d = ImageDraw.Draw(image)
-        bounds = (0, 0, image.width, image.height)
-        batt = self.world.robot.battery_voltage
-        text = cozmo.annotate.ImageText('BATT %.1fv' % batt, color='green')
-        text.render(d, bounds)
-
-# Define a decorator as a subclass of Annotator; displays the ball
-class BallAnnotator(cozmo.annotate.Annotator):
-
-    ball = None
-
-    def apply(self, image, scale):
-        d = ImageDraw.Draw(image)
-        bounds = (0, 0, image.width, image.height)
-
-        if BallAnnotator.ball is not None:
-
-            #double size of bounding box to match size of rendered image
-            BallAnnotator.ball = np.multiply(BallAnnotator.ball,2)
-
-            #define and display bounding box with params:
-            #msg.img_topLeft_x, msg.img_topLeft_y, msg.img_width, msg.img_height
-            box = cozmo.util.ImageBox(BallAnnotator.ball[0]-BallAnnotator.ball[2],
-                                      BallAnnotator.ball[1]-BallAnnotator.ball[2],
-                                      BallAnnotator.ball[2]*2, BallAnnotator.ball[2]*2)
-            cozmo.annotate.add_img_box_to_image(image, box, "green", text=None)
-
-            BallAnnotator.ball = None
-
-class CozmoThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self, daemon=False)
-
-    def run(self):
-        cozmo.robot.Robot.drive_off_charger_on_connect = False  # Cozmo can stay on his charger
-        cozmo.run_program(run, use_viewer=False)
 
 
 if __name__ == '__main__':
